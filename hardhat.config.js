74dde30fce2ace7b40cf1eedd2e2ebb34b5b7e04
karangorania <karangoraniya@gmail.com>
2022-05-31 19:18:08 +0530

    hardhat config added 🚀

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,18 @@
 require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-etherscan');
+require('dotenv').config();
 
-task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+const { RINKEBY_API_URL, PRIVATE_KEY, ETHERSCAN_KEY } = process.env;
 
 module.exports = {
   solidity: '0.8.4',
+  networks: {
+    rinkeby: {
+      url: RINKEBY_API_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_KEY,
+  },
 };
